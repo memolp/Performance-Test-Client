a60cbf6d521f2fa65ddadc27855c59d53cf28cc9
qinguifeng <memolp@163.com>
2019-10-02 15:32:08 +0800

    加入license

--- gui/AboutDialog.py
+++ gui/AboutDialog.py
@@ -28,29 +28,6 @@ SOFTWARE.
 from PyQt5.QtWidgets import *
 from PyQt5.Qt import *
 
-MIT_LICENSE = """MIT License
-
-Copyright (c) 2019 JeffXun
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
 
 
 class VAboutDialog(QDialog):
@@ -65,7 +42,7 @@ class VAboutDialog(QDialog):
         _vLayout.addWidget(QLabel("压测工具\r\n作者:JeffXun\r\n2019-09-30"))
 
         _license_edit = QTextEdit()
-        _license_edit.setText(MIT_LICENSE)
+        _license_edit.setText(__doc__)
         _license_edit.setReadOnly(True)
         _license_edit.setObjectName("edit-license")
         _vLayout.addWidget(_license_edit)
